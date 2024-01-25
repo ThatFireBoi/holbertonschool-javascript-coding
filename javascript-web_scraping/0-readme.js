0f6454edae1a0edfad88c9625eca01f29de3ce97
ThatFireBoi <gcf2007@hotmail.com>
2024-01-25 18:24:46 -0400

    Fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
+/* Script that reads and prints the content of a file. */
 const fs = require('fs');
-const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
