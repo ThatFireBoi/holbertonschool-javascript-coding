73ede2e5bff4dc2150d3aedcff9a45a258b1a1d6
ThatFireBoi <gcf2007@hotmail.com>
2024-01-25 18:32:12 -0400

    Script that display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Script that display the status code of a GET request. */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+}
+);
