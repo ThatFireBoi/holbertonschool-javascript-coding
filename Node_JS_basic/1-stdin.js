6868b22f58dfb71565b1ea99d0bac702b4365604
ThatFireBoi <gcf2007@hotmail.com>
2024-02-02 00:27:54 -0400

    Fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,14 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Ask the user for their name
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // Output the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Close the readline interface
-  rl.close();
-});
-
-// Add a listener for the 'close' event
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
